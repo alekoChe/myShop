79a4a2fb28bfa84f10104550dac9f323f997d4a7
alekoChe <aleko2117bis@gmail.com>
2023-02-20 12:49:09 +0300

    add OrderRepository.java, UserRepository.java, OrderItemRepository.java

---
+++ myShop/src/main/java/com/aleko/myShop/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.aleko.myShop.repositories;
+
+import com.aleko.myShop.entities.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+}
